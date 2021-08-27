0397a7ec62dd41df2b57d5fd8dd71c6f3b840cbc
CEO-MUN <77603287+CEO-MUN@users.noreply.github.com>
2021-08-28 02:15:14 +0900

    commit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,32 +5,40 @@ import NweetFactory from "components/NweetFactory";
 import Nweet from "components/Nweet";
 import DetailContent from "components/DetailContent";
 
-const Detail = ({ userObj }) => {
-  const [nweets, setNweets] = useState([]);
-  useEffect(() => {
-    dbService
-      .collection("nweets")
-      .orderBy("createdAt", "desc")
-      .onSnapshot((snapshot) => {
-        const nweetArray = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setNweets(nweetArray);
-      });
-  }, []);
+const Detail = ({ match }) => {
+  const [data, setData] = useState({});
+  const { no } = match.params;
+  console.log(no + "s");
+
   return (
-    <div className="container">
-      <div style={{ marginTop: 30 }}>
-        {nweets.map((nweet) => (
-          <DetailContent
-            key={nweet.id}
-            nweetObj={nweet}
-            isOwner={nweet.creatorId === userObj.uid}
-          />
-        ))}
+    <>
+      <h2 align="center">게시글 상세정보</h2>
+
+      <div className="post-view-wrapper">
+        <>
+          <div className="post-view-row">
+            <label>게시글 번호</label>
+            <label>{no}</label>
+          </div>
+          <div className="post-view-row">
+            <label>제목</label>
+            <label>{data.title}</label>
+          </div>
+          <div className="post-view-row">
+            <label>작성일</label>
+            <label>{data.createDate}</label>
+          </div>
+          <div className="post-view-row">
+            <label>조회수</label>
+            <label>{data.readCount}</label>
+          </div>
+          <div className="post-view-row">
+            <label>내용</label>
+            <div>{data.content}</div>
+          </div>
+        </>
       </div>
-    </div>
+    </>
   );
 };
 export default Detail;
